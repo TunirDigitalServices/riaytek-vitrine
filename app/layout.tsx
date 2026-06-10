a0697f4c0764d2ccd6f0590e3973bd10a8790e3d
khalil kraiem <khalilmedkraiem@gmail>
2026-06-10 14:39:58 +0100

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Riaytek",
-  description: "Riaytek is a your freindly health assistant, providing personalized health insights and recommendations based on your unique health data.",
- 
+  description:
+    "Riaytek is a your freindly health assistant, providing personalized health insights and recommendations based on your unique health data.",
 };
 
 export default function RootLayout({
@@ -31,7 +31,8 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <header className="w-full border-b border-solid border-zinc-200 bg-white dark:border-zinc-700 dark:bg-black">
-          <div className="max-w-3xl mx-auto px-4 py-6 flex items-center gap-4">
+          <nav className="ml-auto flex items-center gap-6 text-sm font-medium text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-50 transition-colors sm:gap-8 sm:text-base sm:font-normal">
+            <div className="max-w-3xl mx-auto px-4 py-6 flex items-center gap-4">
               <Image
                 className="dark:invert"
                 src="/logo.png"
@@ -40,13 +41,29 @@ export default function RootLayout({
                 height={20}
                 priority
               />
-            <h1 className="text-2xl font-bold text-zinc-900 dark:text-zinc-50">
-             Riaytek
-            </h1>
-          </div>
+            </div>
+            <a
+              href="#"
+              className="text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50 transition-colors"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50 transition-colors"
+            >
+              Features
+            </a>
+            <a
+              href="#"
+              className="text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-50 transition-colors"
+            >
+              Contact
+            </a>
+          </nav>
         </header>
         {children}
-        </body>
+      </body>
     </html>
   );
 }
